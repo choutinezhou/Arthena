6825dbd8b377c3022105acfd64de1693c37a05a0
Qiuting Zhou <qiutingzhou@dyn-160-39-142-238.dyn.columbia.edu>
2018-02-13 00:42:37 -0500

    parse the artist names

--- html_parser.py
+++ html_parser.py
@@ -1,3 +1,21 @@
 # -*- coding: utf-8 -*-
 
+# import libraries
+from bs4 import BeautifulSoup
+import os
+import glob
+import json
+
+path='data/2015-03-18/'
+artists = []
+for htmlfile in glob.glob(os.path.join(path, "*.html")):
+    with open(htmlfile,"r") as f:
+        page = f.read()
+    soup = BeautifulSoup(page,"lxml")
+    head = soup.h2.string
+    artists.append(head)
+
+jarray=json.dumps(artists)
+
+print (artists)
 
