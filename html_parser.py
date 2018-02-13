f255b503eef2f82089808ecaaf9895de0303b678
Qiuting Zhou <qiutingzhou@dyn-160-39-142-238.dyn.columbia.edu>
2018-02-13 00:45:53 -0500

    parse the art titles

--- html_parser.py
+++ html_parser.py
@@ -6,15 +6,26 @@ import os
 import glob
 import json
 
+# identify the directory path
+# parse the 2015-03-18 folder
+
 path='data/2015-03-18/'
+
+# create a list of artists
 artists = []
+
+# open all html files in this folder
 for htmlfile in glob.glob(os.path.join(path, "*.html")):
     with open(htmlfile,"r") as f:
         page = f.read()
     soup = BeautifulSoup(page,"lxml")
+    # parse the artist names
     head = soup.h2.string
-    artists.append(head)
+    # parse the art work titles
+    works=soup.h3.string
+    artists.append({'artist':head,'works':works})
 
+# transform the list to JSON arrays    
 jarray=json.dumps(artists)
 
 print (artists)
